68061d5b06a0a458a4114f77e770a4fc95c86c9c
Guillaume Fay <guillaume.fay@zenika.com>
2017-10-07 22:22:47 +0200

    Add electron-updater

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,5 @@
 const {dialog, BrowserWindow, app, ipcMain, Tray} = require('electron')
+const autoUpdater = require("electron-updater").autoUpdater
 const Puissance4 = require('./puissance4');
 const minMaxWorker = require('child_process').fork(`${__dirname}/minmax`);
 
@@ -7,21 +8,21 @@ let browserWindow, tray, game;
 const createBrowserWindow = () => {
     browserWindow = new BrowserWindow();
     browserWindow.maximize();
-    browserWindow.loadURL(`file://${__dirname}/client/index.html`);
+    browserWindow.loadURL(`file://${__dirname}/../app/index.html`);
 }
 
 const createTray = () => {
-    tray = new Tray(`${__dirname}/client/img/red.png`);
+    tray = new Tray(`${__dirname}/../assets/img/red.png`);
     tray.setToolTip('A vous de jouer.');
 }
 
 const updateTray = (event) => {
     let image, tooltip;
     if (event === 'humanMoved') {
-        image = `${__dirname}/client/img/yellow.png`
+        image = `${__dirname}/../assets/img/yellow.png`
         tooltip = 'A Electron de jouer';
     } else {
-        image = `${__dirname}/client/img/red.png`
+        image = `${__dirname}/../assets/img/red.png`
         tooltip = 'A vous de jouer';
     }
     tray.setImage(image);
@@ -58,11 +59,18 @@ const emitGameUpdate = (event) => {
 app.on('ready', () => {
     createBrowserWindow();
     createTray();
+    autoUpdater.checkForUpdates();
 });
 
 app.on('window-all-closed', () => {
     app.quit();
 });
+
+autoUpdater.on('update-downloaded', (info) => {
+    setTimeout(() => {
+        autoUpdater.quitAndInstall();  
+    }, 5000)
+})
 
 minMaxWorker.on('message', (move) => {
     game.updateBoard(move, 2);
